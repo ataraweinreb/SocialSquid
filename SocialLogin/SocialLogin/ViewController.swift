e9a83187211952d837b747ce896f86525663a21e
Tara Weinreb <36668765+ataraweinreb@users.noreply.github.com>
2019-06-13 12:19:57 -0400

    Add files via upload

---
+++ SocialLogin/SocialLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by Tara Weinreb on 6/13/19.
+//  Copyright © 2019 Tara Weinreb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
